bf504766a95a702c04f645eddb9c56c1a445b2f6
ramkishorem <ramkishore123@gmail.com>
2018-04-01 12:40:27 +0530

    Broke long lines

--- session3/10.py
+++ session3/10.py
@@ -19,17 +19,22 @@ prints
 
     Next up: David
 
-Since there are no statements inside the loop, Python goes back to the for loop line again. It will see if there are any more items inside the list to be looped. There are two more left.
-So, Alice is assigned next to suspect.
+Since there are no statements inside the loop, Python goes 
+back to the for loop line again. It will see if there are 
+any more items inside the list to be looped. There are two 
+more left. So, Alice is assigned next to suspect.
 Print statement prints,
 
     Next up: Alice
 
-Once again, Python goes back to the for-loop line. One more item is left to be looped.
+Once again, Python goes back to the for-loop line. One more 
+item is left to be looped.
 Ben is assigned next to suspect.
 Print statement prints,
 
     Next up: Ben
 
-Back to the loop line, Python will find that there is no more item left. So it leaves the loop and proceeds after the loop block.
+Back to the loop line, Python will find that there is no 
+more item left. So it leaves the loop and proceeds after 
+the loop block.
 """
