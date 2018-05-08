cdf625e7798a5c08bbe4013cec25655e79771527
ramkishorem <ramkishore123@gmail.com>
2018-05-08 08:43:10 +0530

    updated 7-7 exercise

--- session7_file_except/07.py
+++ session7_file_except/07.py
@@ -1,11 +1,41 @@
 """
 Drill 2: Persistent high score board
 
-Remember the high score board drill that we did?
+Write the high_scores module for the following code to execute as intended
 
-> Make it possible for user to log new highscores
-> Display top five highscores when requested
-> Option to close the program.
-> The highscore values should persist even after the program 
-  is closed and restarted.
 """
+
+from high_scores import HighScores
+
+mario = HighScores('Super Mario', 'super-mario.txt')
+
+
+# Execution 1
+mario.post_score('Steve', 3432)
+mario.post_score('Lara', 4376)
+mario.post_score('Mike', 4899)
+
+print(mario.get_top_5())
+# Output
+# [('Mike', 4899), ('Lara', '4376'), ('Steve', 3432)]
+# super-mario.txt #######
+# Steve, 3432
+# Lara, 4376
+# Mike, 4899
+
+
+# Execution 2
+mario.post_score('Wendy', 4122)
+mario.post_score('Tom', 5212)
+mario.post_score('Winston', 4777)
+
+print(mario.get_top_5())
+# Output
+# [('Tom', 5212), ('Mike', 4899), ('Winston', 4777), ('Lara', 4376), ('Wendy', 4122)]
+# super-mario.txt #######
+# Steve, 3432
+# Lara, 4376
+# Mike, 4899
+# Wendy, 4122
+# Tom, 5212
+# Winston, 4777
